f029c7a018508f5c007294d135a358e9e61d6b97
Ramesh B <122330528+rameshb215@users.noreply.github.com>
2023-07-31 10:47:41 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,1 +1,23 @@
 
+var hourHand = document.querySelector(".hour-hand")
+var minHand = document.querySelector(".min-hand")
+var secondHand = document.querySelector(".second-hand")
+
+function setDate() {
+    var now = new Date()
+
+    var hours = now.getHours()
+    var hoursDegrees = (hours / 12) * 360 + 90
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`
+
+    var mins = now.getMinutes()
+    var minsDegrees = (mins / 60) * 360 + 90
+    minHand.style.transform = `rotate(${minsDegrees}deg)`
+
+    var secs = now.getSeconds()
+    var secsDegrees = (secs / 60) * 360 + 90
+    secondHand.style.transform = `rotate(${secsDegrees}deg)`
+}
+
+setInterval(setDate, 1000)
+setDate()
